9098d1b5e5a111dd85d81279b6db4608f36ec24d
Sarwan Kumar <sarwan.30kr@gmail.com>
2024-12-24 11:16:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1); // Gives true
+// console.log("02" > 1); // Gives true
+
+
+// Comparison For Null
+// console.log(null > 0);// false
+// console.log(null < 0);// false
+// console.log(null == 0);// false
+// console.log(null <= 0);// true
+// console.log(null >= 0);// true
+// console.log(null != 0);// true
+
+//Comparison for undefined
+// console.log(undefined > 0);// false
+// console.log(undefined < 0);// false
+// console.log(undefined == 0);// false
+// console.log(undefined <= 0);// false
+// console.log(undefined >= 0);// false
+// console.log(undefined != 0);// true
+
+//Strict Check ===
+//Check the value and their data types as well
+console.log("2" === 2)
