7125a9b7cec7db9ad213ab4a9cc30de4187ef96b
Sarwan Kumar <sarwan.30kr@gmail.com>
2024-12-24 10:41:56 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -98,3 +98,37 @@ const data1 = [
   let stringNumber = String(someNumber)
   console.log(stringNumber)
   console.log(typeof stringNumber)
+
+  /*  ***********************Operations********************* */
+
+
+  let value = 3
+  let negvalue = -value
+//   console.log(negvalue)
+
+//Basic Math Operation
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // for 2 raise to power 2
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Shravan"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true) // gives output as 1
+// console.log(true+) // throw an error
+console.log(+"") // gives 0
+console.log(-false) //gives -0
+console.log(+false)
+
+// Prefix and Postfix operator
