bbe82590e2840d8a5a9a2cde3e184c5d10733411
Sarwan Kumar <sarwan.30kr@gmail.com>
2025-01-26 14:46:10 +0000

    Hi

--- 04_Controlflow.js/01_one.js
+++ 04_Controlflow.js/01_one.js
@@ -1,13 +1,30 @@
 // if
 const isUserLoggedIn = true
+const temp = 41
 
 if (isUserLoggedIn) {
-    
 }
 
-if(1 === "One"){
+if(1 == "1"){
     console.log("Executed");
-
 }
+
+if(temp < 50){
+    console.log("Temperature less than 50");
+}else{
+    console.log("Temperature Greater than 50");
+}
+
+// Short hand notation
+// Implicit Scope
+const bal = 1000;
+// if (bal > 500) console.log("test"), console.log("test2"),
+// console.log("test3");
+
+
+
+
+
+
 
 // <, >, <=, >=, ==, !=, ===
---
+++ 04_Controlflow.js/02_Switch.js
@@ -0,0 +1,15 @@
+const month = 5;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2: 
+        console.log("February");
+        break;
+    case 3: 
+        console.log("March");
+        break;
+    default: 
+        console.log("Happy Birthday");
+        break;
+}
